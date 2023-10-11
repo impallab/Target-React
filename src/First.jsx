5ad86ad2c0603854ec81550c3c6d7820bfc70caf
impallab <pallab13nayagram@gamil.com>
2023-10-11 10:27:47 +0530

    almost done with this

---
+++ src/First.jsx
@@ -0,0 +1,21 @@
+function FirstCode(){
+    return(
+        <>
+        <h2>writing my first react code under react application</h2>
+        <table>
+            <tr>
+                <th>Name</th>
+                <th>Mobile No:</th>
+            </tr>
+            
+            <tr>
+                <td>Pallab</td>
+                <td>9641111733</td>
+            </tr>
+        </table>
+        <a href="https://pallab-portfolio.netlify.app/">PORTFOLIO</a>
+        </>
+    )
+}
+
+export default FirstCode
